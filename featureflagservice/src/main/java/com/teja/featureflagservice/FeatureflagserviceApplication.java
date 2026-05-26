1c6662578bb4a8114b2652de81d224cd7bd06115
dorateja293 <dorateja293@users.noreply.github.com>
2026-05-26 10:50:02 +0530

    Initial Spring Boot project setup

---
+++ featureflagservice/src/main/java/com/teja/featureflagservice/FeatureflagserviceApplication.java
@@ -0,0 +1,13 @@
+package com.teja.featureflagservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureflagserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureflagserviceApplication.class, args);
+	}
+
+}
